dccff3aa3b0bcd355e31632b486bdfe1c10129db
Alex Madsen <akmadsen522@gmail.com>
2014-01-23 22:21:36 -0700

    Added Current Set of Game Files

---
+++ FPS Tutorial/Assets/Scripts/FP_Shooter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FP_Shooter : MonoBehaviour
+{
+
+	public GameObject bullet_prefab;
+	float bulletImpulse = 20f; 
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1")) {	
+			Camera cam = Camera.main; 
+			GameObject thebullet = (GameObject) Instantiate(bullet_prefab, cam.transform.position, cam.transform.rotation); 
+			thebullet.rigidbody.AddForce(cam.transform.forward * bulletImpulse, ForceMode.Impulse); 
+		}
+	}
+}
---
+++ FPS Tutorial/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(CharacterController))] 
+public class FirstPersonController : MonoBehaviour {
+
+	public float movementspeed = 5.0f; 
+	public float mouseSensitivity = 5.0f; 
+	public float jumpSpeed = 20.0f; 
+
+	float verticalRotation = 0; 
+	public float upDownRange = 60.0f; 
+
+	float verticalVelocity = 0; 
+
+	CharacterController characterController; 
+
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true; 
+		characterController = GetComponent<CharacterController> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Rotation  
+		float rotLeftRight = Input.GetAxis ("Mouse X") * mouseSensitivity; 
+		transform.Rotate (new Vector3 (0, rotLeftRight, 0)); 
+
+		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSensitivity; 
+		verticalRotation = Mathf.Clamp (verticalRotation, -upDownRange, upDownRange); 
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0); 
+
+
+		// Movement 
+
+		float forwardSpeed = Input.GetAxis( "Vertical" ) * movementspeed; 
+		float sideSpeed = Input.GetAxis ("Horizontal") * movementspeed; 
+
+		verticalVelocity += Physics.gravity.y * Time.deltaTime; 
+
+		// "Did the Jump button go down since the last frame?" 
+		if (characterController.isGrounded && Input.GetButtonDown ("Jump")) {
+			verticalVelocity = jumpSpeed; 
+		}
+
+		Vector3 speed = new Vector3 ( sideSpeed, verticalVelocity, forwardSpeed ); 
+
+		speed = transform.rotation * speed; 
+
+		characterController.Move (speed * Time.deltaTime);  
+
+	}
+}
